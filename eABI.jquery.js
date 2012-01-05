67ec5cd974543cc9a319da5c3d843272c8697156
Johnny Martin <johnny@johnny5th.com>
2012-01-05 14:32:21 -0600

    First Working Version

---
+++ eABI.jquery.js
@@ -0,0 +1,41 @@
+(function( $ ){
+
+	$.fn.eABI = function(options) {  
+	
+		var settings = $.extend({
+			'data' : '',
+			condition : function(){},
+			callback : function(){}
+		}, options);
+		
+		return this.each(function() {
+			
+			var $this = $(this);
+			
+			
+			// Check condition on load
+			if(settings.condition() == true){
+				
+				$this.append(data);
+				
+				$this.addClass('activecontent');
+				
+				settings.callback();
+				
+			}
+			
+			// Check for the condition on page resize
+			$(window).resize(function() {
+				if(!$this.hasClass('activecontent') && settings.condition()){
+					$this.append(data);
+					
+					$this.addClass('activecontent');
+					
+					settings.callback();
+				}
+			});
+			
+		});
+	
+	};
+})( jQuery );
